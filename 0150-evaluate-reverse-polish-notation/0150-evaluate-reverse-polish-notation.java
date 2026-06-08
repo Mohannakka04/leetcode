88b8fb95d30026c09da9b531ec9df6f84774231b
Mohannakka04 <nakka7321@gmail.com>
2026-06-08 15:46:19 +0530

    Time: 6 ms (89.6%), Space: 45.4 MB (27.73%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,6 +1,6 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> s = new Stack<>();
+        Deque<Integer> s = new ArrayDeque<>();
         for(int i=0;i<tokens.length;i++)
         {
             if(tokens[i].equals("*"))
