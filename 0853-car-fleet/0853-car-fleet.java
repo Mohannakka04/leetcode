797c8d32be2a3f8a01ae28e31df29ac726553c98
Mohannakka04 <nakka7321@gmail.com>
2026-06-15 12:29:21 +0530

    Time: 93 ms (24.55%), Space: 95.7 MB (74.23%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        Deque<double[]> s = new ArrayDeque<>();
+        for(int i=0;i<n;i++)
+        {
+            double time =(double) (target-position[i])/speed[i];
+            s.push(new double[]{position[i],time});
+        }
+        List<double[]> list = new ArrayList<>(s);
+
+        list.sort((a, b) -> Double.compare(a[0], b[0]));
+
+        s.clear();
+        for (double[] car : list) {
+            s.push(car);
+        }
+
+        int count = 0;
+        double lastTime = -1;
+        while(!s.isEmpty())
+        {
+            double currTime = s.pop()[1];
+
+            if (lastTime == -1 || currTime > lastTime) {
+                count++;
+                lastTime = currTime;
+            }
+        }
+        return count;
+    }
+}
